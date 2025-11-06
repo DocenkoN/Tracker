9c9dd4ceaf7cf5abff67145eee6e854b3c63cd4b
DocenkoN <>
2025-11-06 14:33:03 +0300

    первый коммит

---
+++ Tracker_New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker_New
+//
+//  Created by Nikolay on 06.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tracker_NewTests/Tracker_NewTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_NewTests.swift
+//  Tracker_NewTests
+//
+//  Created by Nikolay on 06.11.2025.
+//
+
+import Testing
+@testable import Tracker_New
+
+struct Tracker_NewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
